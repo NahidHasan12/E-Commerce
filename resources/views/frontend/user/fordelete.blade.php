2ebb1307fdf3863b02f757dd636855e1415f8f98
NahidHasan12 <nhd3456555@gmail.com>
2023-11-21 22:14:59 +0600

    for delete

--- resources/views/frontend/user/fordelete.blade.php
+++ resources/views/frontend/user/fordelete.blade.php
@@ -53,50 +53,6 @@
             soluta voluptas vero et suscipit commodi laudantium unde, possimus dolorem eveniet
             architecto nesciunt?
  </p>
- <p>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident, qui fugiat tempore rem
-    tenetur modi veritatis quod sit quidem ab, tempora laudantium perferendis iste fugit odio
-    consectetur, quo ut deserunt maiores voluptatum doloribus sequi! Reiciendis, consequatur?
-    Asperiores, necessitatibus ut quia porro ipsam vitae minus reiciendis, perferendis quidem quam
-    consequatur quas quis ea vel? Aut ab quam exercitationem ut fuga, accusantium libero odio
-    molestias asperiores, sunt minima nisi rerum labore laboriosam nulla assumenda vitae harum?
-    Explicabo optio velit nihil, itaque id nostrum, ullam praesentium asperiores enim aliquam
-    pariatur cumque repellat nobis quas. Porro asperiores eaque amet eveniet, explicabo consequuntur
-    adipisci molestiae magnam, nemo libero velit nulla ducimus nihil facere? Vel assumenda, velit
-    incidunt quaerat, voluptate magni dignissimos reprehenderit excepturi iste corporis inventore!
-    Veritatis, accusantium debitis quam dolorem ut fugit ratione sapiente quos rerum sequi, officiis
-     sit illo! Magni, iste, deleniti cupiditate ullam blanditiis odit delectus sunt quaerat placeat
-     dolorum quidem quo aliquam in rerum nulla provident nihil eos. Sapiente adipisci ipsum eos
-      fugiat voluptas eum reprehenderit id beatae voluptatem dolore neque officia, sint temporibus
-       cum. Officiis, non odio. Eligendi architecto possimus cupiditate provident tempore fugit
-        magni enim assumenda vero? Maiores nulla velit quasi dolorum nihil asperiores aperiam amet
-    tempore eos. Enim unde eligendi voluptatum aut quod eaque ipsum atque possimus qui pariatur
-    nam quos, illum voluptate nobis voluptas nisi labore blanditiis odio. Veniam repudiandae,
-    aut placeat, odit pariatur perferendis non mollitia quae quaerat similique dolorum saepe
-    quas quibusdam possimus, praesentium aliquid. Corrupti iste ipsum cumque numquam odio
-    beatae aspernatur. Temporibus minus ut maiores totam modi aperiam? Eaque architecto a
-    alias beatae nulla voluptate voluptatibus suscipit neque omnis incidunt, reprehenderit
-    nostrum. Impedit cupiditate tempora, ipsam dolores sit officia eum possimus dignissimos
-        ullam nulla reiciendis praesentium. Natus suscipit atque cum ex commodi est, incidunt
-        sapiente autem quisquam nostrum id beatae ipsum sequi tenetur reprehenderit, pariatur,
-        enim nam? Consequuntur alias ea in, est eos exercitationem saepe id. Provident, ratione
-        ab labore quia nostrum consequatur consectetur temporibus a dolor aliquid ipsa ducimus,
-        dolores rem inventore consequuntur excepturi obcaecati illo dolorum recusandae numquam
-        esse voluptatem itaque architecto. Blanditiis alias quo eveniet esse provident minima
-        tenetur eos, architecto nemo natus porro dolor numquam excepturi eligendi asperiores
-        atque impedit dolores pariatur culpa sit. Ducimus nulla rerum est asperiores et atque
-        expedita placeat voluptate dolore deserunt, voluptates saepe ipsam repudiandae aliquid
-        dolorem a quaerat. Animi aspernatur quaerat voluptatem quasi similique, officia ad,
-        optio blanditiis vel magni soluta, eius nemo hic illo illum esse? Ipsa odit sed, dolor
-        autem, minima, neque nesciunt iusto minus nemo earum maxime praesentium? Eligendi,
-        labore earum, ipsa ea qui sit neque, nihil amet mollitia quis id ipsum. Provident
-        voluptate possimus quibusdam iste suscipit ad quo, distinctio recusandae odit aliquid,
-        unde autem quae doloremque ducimus error magnam alias repellendus velit temporibus
-        officiis corporis, hic porro rem aspernatur. Fugit distinctio impedit incidunt
-            eos eaque molestiae, earum vero possimus quae aut laborum cupiditate, soluta
-            veritatis ut commodi est dolorem quidem repellat? Velit doloribus tempora quaerat
-            soluta voluptas vero et suscipit commodi laudantium unde, possimus dolorem eveniet
-            architecto nesciunt?
- </p>
+
 </body>
 </html>
