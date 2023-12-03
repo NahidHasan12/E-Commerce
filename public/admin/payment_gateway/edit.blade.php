cb65c3d7e4a26fc04244a4832867b0d3036e441f
NahidHasan12 <nhd3456555@gmail.com>
2023-12-03 14:42:40 +0600

    for delete

--- public/admin/payment_gateway/edit.blade.php
+++ public/admin/payment_gateway/edit.blade.php
@@ -1,3 +1,9 @@
 <h1> Payment Gateway</h1>
 <h1> Payment Gateway</h1>
 <h1> Payment Gateway</h1>
+<h1> Payment Gateway</h1>
+<h1> Payment Gateway</h1>
+<h1> Payment Gateway</h1>
+<h1> Payment Gateway</h1>
+<h1> Payment Gateway</h1>
+<h1> Payment Gateway</h1>
