7ee4dbebc3625b9fa6f40e1ede605085824cc233
NahidHasan12 <nhd3456555@gmail.com>
2023-12-09 21:49:36 +0600

    for demo

--- public/admin/payment_gateway/edit.blade.php
+++ public/admin/payment_gateway/edit.blade.php
@@ -2,9 +2,6 @@
 <h1> Payment Gateway</h1>
 <h1> Payment Gateway</h1><h1> Payment Gateway</h1>
 <h1> Payment Gateway</h1><h1> Payment Gateway</h1>
-<h1> Payment Gateway</h1><h1> Payment Gateway</h1>
-<h1> Payment Gateway</h1><h1> Payment Gateway</h1>
-<h1> Payment Gateway</h1><h1> Payment Gateway</h1>
 
 
 
